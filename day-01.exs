53609218641aa14cb7c0cb4a7ec158b9abc76d18
Zac Wasielewski <zac@wasielewski.org>
2019-12-01 19:27:37 -0500

    Add documentation for Day 1 functions

--- day-01.exs
+++ day-01.exs
@@ -1,8 +1,21 @@
 defmodule FuelCounterUpper do
+  @doc """
+  Fuel required to launch a given module is based on its mass.
+  Specifically, to find the fuel required for a module, take its
+  mass, divide by three, round down, and subtract 2.
+  """
   def fuel_required(mass) do
     trunc(mass / 3) - 2
   end
 
+  @doc """
+  Fuel itself requires fuel just like a module. However, that fuel
+  also requires fuel, and that fuel requires fuel, and so on.
+  So, for each module mass, calculate its fuel and add it to the 
+  total. Then, treat the fuel amount you just calculated as the
+  input mass and repeat the process, continuing until a fuel
+  requirement is zero or negative.
+  """
   def total_fuel_required(module_mass) do
     Stream.iterate(module_mass, &fuel_required/1)
       |> Enum.take_while(fn x -> x > 0 end)
