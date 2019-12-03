8079b44b796d57359b5273311e0e2cf2eb21b227
Zac Wasielewski <zac@wasielewski.org>
2019-12-03 00:37:53 -0500

    Functional and super ugly Day 2, Part 2

--- day-02.exs
+++ day-02.exs
@@ -13,9 +13,13 @@ defmodule Day2 do
   end
 
   defp reset_to_1202_program_alarm_state(intcode) do
+    reset_program_state(intcode, 12,  2)
+  end
+
+  defp reset_program_state(intcode, first, second) do
     intcode
-    |> List.replace_at(1, 12)
-    |> List.replace_at(2, 2)
+    |> List.replace_at(1, first)
+    |> List.replace_at(2, second)
   end
 
   defp get_opcode(intcode, opcode_pos) do
@@ -57,12 +61,12 @@ defmodule Day2 do
     # Opcodes are positioned at every 4th item of the intcode (beginning at 1)
     opcode_positions = Stream.take_every(0..length(intcode), 4)
 
-    Enum.reduce_while(opcode_positions, intcode, fn pos, intcode_acc ->
-      opcode = get_opcode(intcode_acc, pos)
+    Enum.reduce_while(opcode_positions, intcode, fn pos, acc ->
+      opcode = get_opcode(acc, pos)
       case opcode do
-        1  -> {:cont, process_opcode(1, intcode_acc, pos) }
-        2  -> {:cont, process_opcode(2, intcode_acc, pos) }
-        99 -> {:halt, intcode_acc}
+        1  -> {:cont, process_opcode(1, acc, pos) }
+        2  -> {:cont, process_opcode(2, acc, pos) }
+        99 -> {:halt, acc}
       end
     end)
   end
@@ -70,9 +74,35 @@ defmodule Day2 do
   @doc """
   """
   def part1 do
-    intcode = parse_intcode_string(get_input())
-    run_intcode(reset_to_1202_program_alarm_state(intcode))
+    intcode = reset_to_1202_program_alarm_state(parse_intcode_string(get_input()))
+    run_intcode(intcode)
+  end
+
+  def part2 do
+    intcode_string = parse_intcode_string(get_input())
+
+    # Generate a list of noun/verb pairs to tet
+    test_inputs = Enum.flat_map(1..100, fn noun ->
+      Enum.map(1..100, fn verb ->
+        [noun, verb]
+      end)
+    end)
+
+    test_inputs
+    |> Enum.map(fn inputs ->
+      [noun, verb] = inputs
+      intcode = reset_program_state(intcode_string, noun, verb)
+      %{
+        noun: noun,
+        verb: verb,
+        output: List.first(run_intcode(intcode)),
+        solution: noun * 100 + verb
+      }
+    end)
+    |> Enum.filter(fn x -> x[:output] === 19690720 end)
   end
 end
 
-IO.puts "Part 1: #{Enum.join(Day2.part1, ",")}"
+IO.puts "Part 1: #{List.first(Day2.part1)}"
+IO.puts "Part 2:"
+IO.inspect Day2.part2
